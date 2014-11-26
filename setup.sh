3ffc8b1efa483ffd07c5a9b573dcfb325fea438d
Geoffrey Lessel <geo@ministrycentered.com>
2014-11-26 10:01:09 -0600

    Do initial setup of vim plugins in setup

--- setup.sh
+++ setup.sh
@@ -3,3 +3,6 @@ ln -s bin/dotfiles/vim .vim
 ln -s bin/dotfiles/vim/vimrc .vimrc
 echo "source-file ${1}/bin/dotfiles/tmux.conf" > .tmux.conf
 echo "source ${1}/bin/dotfiles/zshrc" > .zshrc
+
+git clone https://github.com/gmarik/Vundle.vim.git ${1}/.vim/bundle/Vundle.vim
+echo "Inside vim, run :PluginInstall to install plugins"
