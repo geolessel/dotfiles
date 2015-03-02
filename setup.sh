deb78a74b1040804096b11daf2dda8eab9eb13ed
Geoffrey Lessel <geo@ministrycentered.com>
2015-03-02 10:05:02 -0600

    Added ctags git hooks

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 cd $1
 ln -s bin/dotfiles/vim .vim
 ln -s bin/dotfiles/vim/vimrc .vimrc
+ln -s bin/dotfiles/git_template .git_template
 echo "source-file ${1}/bin/dotfiles/tmux.conf" > .tmux.conf
 echo "source ${1}/bin/dotfiles/zshrc" > .zshrc
 
