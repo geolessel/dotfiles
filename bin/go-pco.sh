5938a60923da4a4b8e9502dd92e43cf69180bb46
Geoffrey Lessel <geo@geoffreylessel.com>
2021-11-18 13:52:27 -0800

    change: update go-pco.sh with new workspace requirements

--- bin/go-pco.sh
+++ bin/go-pco.sh
@@ -3,15 +3,15 @@
 echo 'Starting pco-box...'
 box start
 
-echo 'Starting eslint_d'
+echo 'Starting eslint_d...'
 eslint_d start
 
-cd ~/Code/people
+cd ~/Code/
 tmux new-session -s pco -d
 tmux split-window -h -t 1
-tmux new-window -t pco -n logs
+# tmux new-window -t pco -n logs
 tmux new-window -t pco -n honcho
-tmux send-keys -t pco:2 'tail -f log/development.log' C-m
-tmux send-keys -t pco:3 'cd ../all-the-procfiles && froman -c honcho.yml' C-m
+# tmux send-keys -t pco:2 'tail -f log/development.log' C-m
+tmux send-keys -t pco:2 'cd all-the-procfiles && froman -c honcho.yml' C-m
 tmux select-window -t pco:1
 tmux attach -t pco
