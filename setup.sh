2b9bafc8dbb80c925588ea16ec4b2dc2719b0651
Geoffrey Lessel <geo@geoffreylessel.com>
2021-11-19 09:55:08 -0800

    feat(zsh): add p10k config

--- setup.sh
+++ setup.sh
@@ -10,6 +10,7 @@ ln -s usr/dotfiles/emacs/emacs .emacs
 ln -s usr/dotfiles/emacs/emacs.d .emacs.d
 echo "source-file ${1}/usr/dotfiles/tmux.conf" > .tmux.conf
 echo "source ${1}/usr/dotfiles/zshrc" > .zshrc
+ln -s usr/dotfiles/zsh/p10k.zsh .p10k.zsh
 
 # git clone https://github.com/gmarik/Vundle.vim.git ${1}/.vim/bundle/Vundle.vim
 # echo "Inside vim, run :PluginInstall to install plugins"
